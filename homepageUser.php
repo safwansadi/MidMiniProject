9aa7819a3706f1a1b586c2d37bdf8b83f3673b17
safwan <safwaniftekher@gmail.com>
2020-08-10 13:53:50 +0600

    homepage

---
+++ homepageUser.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Page of Simple User</title>
+</head>
+<body>
+    <form>
+        
+        <table style="text-align:center">
+            
+            <tr>
+                <td style="text-align:center">
+                    <h1>Welcome Anny! </h1>
+                </td>
+            </tr>
+            <tr>
+                <td style="text-align:center"><a href="">profile</a></td>
+            </tr>
+            <tr>
+                <td style="text-align:center"><a href="">change password</a></td>
+            </tr>
+            <tr>
+                <td style="text-align:center"><a href="./login.php">log out</a></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ homepahe.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Page</title>
+</head>
+<body>
+    <form>
+        
+        <table>
+            <tr>
+                <td>
+                    <h1> Login as admin </h1>
+                </td>
+            </tr>
+            <tr>
+                <td><a href="">profile</a></td>
+            </tr>
+            <tr>
+                <td><a href="">change password</a></td>
+            </tr>
+            <tr>
+                <td><a href="">view users</a></td>
+            </tr>
+            <tr>
+                <td><a href="./login.php">log out</a></td>
+            </tr>
+            
+        </table>
+    </form>
+</body>
+</html>
